b6aa8766013543907e5cc8027978eb87b0acf166
Patrick Hartling <patrick.hartling@gmail.com>
2003-09-09 03:26:33 +0000

    Initial import of a tool that I am calling Sharppy.  This reads C++ class declarations and generates CLI P/Invoke bridging code so that .NET languages can interoperate completely (I hope) with the native C++ code. This is based on the latest CVS version of Pyste.
    At this point, Sharppy can generate the C++ code needed to deal with
    virtual functions that can be overridden by C# subclasses and the C wrappers
    for the given class' constructor(s) and destructor.  I figured that now was
    a good time to get some revision history built up.
    
    
    git-svn-id: file:///Users/patrick/nobackup/aeon-svn/trunk/sharppy@25 f34b864f-2b4e-4307-96f1-59446bce9074

---
+++ src/exporters.py
@@ -0,0 +1,7 @@
+# This is derived from the Pyste version of exporters.py.
+# See http://www.boost.org/ for more information.
+
+# a list of Exporter instances
+exporters = []
+
+current_interface = None # the current interface file being processed
---
+++ src/settings.py
@@ -0,0 +1,11 @@
+# This is derived from the Pyste version of settings.py.
+# See http://www.boost.org/ for more information.
+
+#==============================================================================
+# Global information
+#==============================================================================
+
+DEBUG = False
+
+class namespaces:
+    sharppy = ''
