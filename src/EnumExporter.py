2251df354903392c05db628472868161f88b1644
Patrick Hartling <patrick.hartling@gmail.com>
2003-11-03 23:36:10 +0000

    Reduce indentation to 3 spaces from 4.
    git-svn-id: file:///Users/patrick/nobackup/aeon-svn/trunk/sharppy@87 f34b864f-2b4e-4307-96f1-59446bce9074

--- src/EnumExporter.py
+++ src/EnumExporter.py
@@ -1,32 +1,29 @@
 # This is derived from the Pyste version of EnumExporter.py.
 # See http://www.boost.org/ for more information.
 
+# $Id: EnumExporter.py,v 1.3 2003-11-03 23:36:09 patrick Exp $
+
 from Exporter import Exporter
-from settings import *
-import utils
 
 #==============================================================================
-# EnumExporter 
+# EnumExporter
 #==============================================================================
 class EnumExporter(Exporter):
-    'Exports enumerators'
+   'Exports enumerators'
 
-    def __init__(self, info):
-        Exporter.__init__(self, info)
+   def __init__(self, info):
+      Exporter.__init__(self, info)
 
+   def SetDeclarations(self, declarations):
+      Exporter.SetDeclarations(self, declarations)
+      if self.declarations:
+         self.enum = self.GetDeclaration(self.info.name)
+      else:
+         self.enum = None
 
-    def SetDeclarations(self, declarations):
-        Exporter.SetDeclarations(self, declarations)
-        if self.declarations:
-            self.enum = self.GetDeclaration(self.info.name)
-        else:
-            self.enum = None
+   def Export(self, exported_names):
+      if not self.info.exclude:
+         exported_names[self.Name()] = 1
 
-
-    def Export(self, exported_names):
-        if not self.info.exclude:
-            exported_names[self.Name()] = 1
-
-
-    def Name(self):
-        return self.info.name
+   def Name(self):
+      return self.info.name
