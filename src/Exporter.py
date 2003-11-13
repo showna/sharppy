d9b2ded3d65080b6a9695a76fa9595c675d106cb
Patrick Hartling <patrick.hartling@gmail.com>
2003-11-13 20:12:22 +0000

    Changed indentation level from 4 spaces to 3.  No functional changes.
    git-svn-id: file:///Users/patrick/nobackup/aeon-svn/trunk/sharppy@122 f34b864f-2b4e-4307-96f1-59446bce9074

--- src/Exporter.py
+++ src/Exporter.py
@@ -1,111 +1,110 @@
 # This is derived from the Pyste version of Exporter.py.
 # See http://www.boost.org/ for more information.
 
+# $Id: Exporter.py,v 1.8 2003-11-13 20:12:22 patrick Exp $
+
 import os
 
 #==============================================================================
 # Exporter
 #==============================================================================
 class Exporter(object):
-    'Base class for objects capable to generate boost.python code.'
+   'Base class for objects capable to generate boost.python code.'
 
-    INDENT = ' ' * 3
-    
-    def __init__(self, info, parser_tail=None, module = 'Unknown'):
-        self.info = info
-        self.parser_tail = parser_tail
-        self.interface_file = None
-        self.declarations = []
-        self.includes = []
-        self.module = module
-        self.cxx_dir = self.module + '_cpp'
-        self.csharp_dir = self.module + '_cs'
+   INDENT = ' ' * 3
 
-    def Name(self):
-        raise NotImplementedError(self.__class__.__name__)
+   def __init__(self, info, parser_tail=None, module = 'Unknown'):
+      self.info = info
+      self.parser_tail = parser_tail
+      self.interface_file = None
+      self.declarations = []
+      self.includes = []
+      self.module = module
+      self.cxx_dir = self.module + '_cpp'
+      self.csharp_dir = self.module + '_cs'
 
-    def Tail(self):
-        return self.parser_tail
+   def Name(self):
+      raise NotImplementedError(self.__class__.__name__)
 
-    def Parse(self, parser):
-        self.parser = parser
-        header = self.info.include
-        tail = self.parser_tail
-        declarations, parser_header = parser.parse(header, tail)
-        self.parser_header = parser_header
-        self.SetDeclarations(declarations)
+   def Tail(self):
+      return self.parser_tail
 
-    def SetParsedHeader(self, parsed_header):
-        if parsed_header not in self.includes:
-            self.includes.append(parsed_header)
+   def Parse(self, parser):
+      self.parser = parser
+      header = self.info.include
+      tail = self.parser_tail
+      declarations, parser_header = parser.parse(header, tail)
+      self.parser_header = parser_header
+      self.SetDeclarations(declarations)
 
-    def setModule(self, module):
-        self.module = module
+   def SetParsedHeader(self, parsed_header):
+      if parsed_header not in self.includes:
+         self.includes.append(parsed_header)
 
-    def setOutputDirs(self, cxx, csharp):
-        if os.access(cxx, os.F_OK):
-            if not os.access(cxx, os.R_OK):
-                # XXX: Need to throw an exception here.
-                print 'C++ Crap'
-        else:
-            os.mkdir(cxx)
+   def setModule(self, module):
+      self.module = module
 
-        self.cxx_dir = cxx
+   def setOutputDirs(self, cxx, csharp):
+      if os.access(cxx, os.F_OK):
+         if not os.access(cxx, os.R_OK):
+            # XXX: Need to throw an exception here.
+            print 'C++ Crap'
+      else:
+         os.mkdir(cxx)
 
-        if os.access(csharp, os.F_OK):
-            if not os.access(csharp, os.R_OK):
-                # XXX: Need to throw an exception here.
-                print 'C# Crap'
-        else:
-            os.mkdir(csharp)
+      self.cxx_dir = cxx
 
-        self.csharp_dir = csharp
+      if os.access(csharp, os.F_OK):
+         if not os.access(csharp, os.R_OK):
+            # XXX: Need to throw an exception here.
+            print 'C# Crap'
+      else:
+         os.mkdir(csharp)
 
-    def SetDeclarations(self, declarations):
-        self.declarations = declarations
+      self.csharp_dir = csharp
 
-    def GenerateCode(self, exported_names):
-        self.Export(exported_names)        
-        self.Write()        
+   def SetDeclarations(self, declarations):
+      self.declarations = declarations
 
-    def Export(self, exported_names):
-        'Subclasses must override this to do the real work.'
-        pass
-    
-    def Write(self):
-        'Subclasses must override this to do the real work.'
-        pass
+   def GenerateCode(self, exported_names):
+      self.Export(exported_names)
+      self.Write()
 
-    def GetDeclarations(self, fullname):
-        decls = []
-        for decl in self.declarations:
-            if decl.FullName() == fullname:
-                decls.append(decl)
-        if not decls:
-            raise RuntimeError, 'no %s declaration found!' % fullname
-        return decls
+   def Export(self, exported_names):
+      'Subclasses must override this to do the real work.'
+      pass
 
+   def Write(self):
+      'Subclasses must override this to do the real work.'
+      pass
 
-    def GetDeclaration(self, fullname):
-        decls = self.GetDeclarations(fullname)
-        #assert len(decls) == 1
-        return decls[0]
+   def GetDeclarations(self, fullname):
+      decls = []
+      for decl in self.declarations:
+         if decl.FullName() == fullname:
+            decls.append(decl)
+      if not decls:
+         raise RuntimeError, 'no %s declaration found!' % fullname
+      return decls
 
+   def GetDeclaration(self, fullname):
+      decls = self.GetDeclarations(fullname)
+      #assert len(decls) == 1
+      return decls[0]
 
-    def Order(self):
-        '''Returns a string that uniquely identifies this instance. All
-        exporters will be sorted by Order before being exported.
-        '''
-        return 0, self.info.name
+   def Order(self):
+      '''
+      Returns a string that uniquely identifies this instance. All
+      exporters will be sorted by Order before being exported.
+      '''
+      return 0, self.info.name
 
+   def Header(self):
+      return self.info.include
 
-    def Header(self):
-        return self.info.include
+   def __eq__(self, other):
+      return type(self) is type(other) and self.Name() == other.Name() \
+             and self.interface_file == other.interface_file
 
-
-    def __eq__(self, other):
-        return type(self) is type(other) and self.Name() == other.Name() \
-            and self.interface_file == other.interface_file
-
-    def __ne__(self, other):
-        return not self == other
+   def __ne__(self, other):
+      return not self == other
