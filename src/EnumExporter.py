b323795869e5efe12490231ba5f061abfa6a57d9
Patrick Hartling <patrick.hartling@gmail.com>
2004-02-05 17:44:40 +0000

    Bring this file back from the dead.  Even if free enumerations cannot be exported using this exporter, nested enumerations can still make use of it.
    git-svn-id: file:///Users/patrick/nobackup/aeon-svn/trunk/sharppy@265 f34b864f-2b4e-4307-96f1-59446bce9074

---
+++ src/EnumExporter.py
@@ -0,0 +1,29 @@
+# This is derived from the Pyste version of EnumExporter.py.
+# See http://www.boost.org/ for more information.
+
+# $Id: EnumExporter.py,v 1.6 2004-02-05 17:44:40 patrick Exp $
+
+import Exporter
+
+#==============================================================================
+# EnumExporter
+#==============================================================================
+class EnumExporter(Exporter.Exporter):
+   'Exports enumerators'
+
+   def __init__(self, info):
+      Exporter.Exporter.__init__(self, info)
+
+   def SetDeclarations(self, declarations):
+      Exporter.Exporter.SetDeclarations(self, declarations)
+      if self.declarations:
+         self.enum = self.GetDeclaration(self.info.name)
+      else:
+         self.enum = None
+
+   def Export(self, exported_names):
+      if not self.info.exclude:
+         exported_names[self.Name()] = 1
+
+   def Name(self):
+      return self.info.name
